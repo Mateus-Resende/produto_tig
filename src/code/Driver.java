17dcb587ce84922430c2144d8884add2b6ea60fa
Mateus-Resende <mateus.mendonca.resende@gmail.com>
2015-05-10 21:45:14 -0300

    Comecei a fazer o código. Podem fuçar o tanto que quiserem.

--- src/code/Driver.java
+++ src/code/Driver.java
@@ -1,17 +1,13 @@
 package code;
 
-import java.util.ArrayList;
-
-import dic.Dicionario;
-
 public class Driver {
 
 	public static void main(String[] args) {
-		Dicionario dic = new Dicionario();
-		ArrayList<String> palavras = dic.construirDic();
-		System.out.println(palavras.get(0));
-		System.out.println(palavras.get(palavras.size()-1));
-
+		Procura p = new Procura();
+		System.out.println(29451%29452);
+		String[] resultado = p.procuraBinaria("ligth");
+		System.out.println("Encontramos a palavra " + resultado[0] + " em " + resultado[1] + " nanosegundos.");
+		
 	}
 
 }
